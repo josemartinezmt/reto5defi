9dc396fea1898ca47a8f3b8c89a8c32c36ba455c
Leonardo <jose.martinez.mt@correo.usa.edu.co>
2021-10-28 22:38:33 -0500

    first commit

---
+++ Reto3/src/main/java/Domingo_Reto3/Reto3/InterfaceAudience.java
@@ -0,0 +1,12 @@
+
+package Domingo_Reto3.Reto3;
+
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ *
+ * @author USUARIO
+ */
+public interface InterfaceAudience extends CrudRepository<Audience,Integer> {
+    
+}
